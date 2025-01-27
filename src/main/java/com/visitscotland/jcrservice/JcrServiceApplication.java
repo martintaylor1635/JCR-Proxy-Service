71ae042d51587f299647d2f909ee3376046a333e
martintaylor1635 <martintaylor1635@gmail.com>
2025-01-27 21:43:30 +0000

    Initial Commit

---
+++ src/main/java/com/visitscotland/jcrservice/JcrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.visitscotland.jcrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcrServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcrServiceApplication.class, args);
+    }
+
+}
